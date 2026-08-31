d4970656f5a353eb7c94a42f04f76ba7547cac88
Marcin Król <kroolik89@gmail.com>
2026-08-31 17:51:38 +0200

    Bump version to 0.1.4, set init to false, and simplify run.sh

--- x2iot/run.sh
+++ x2iot/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bashio
+#!/bin/sh
 
 export X2IOT_CONFIG_PATH="/data/configuration.yaml"
 export LEPTOS_SITE_ADDR="0.0.0.0:8356"
@@ -6,6 +6,6 @@ export LEPTOS_SITE_ROOT="/app/site"
 export LEPTOS_OUTPUT_NAME="x2iot"
 export LEPTOS_SITE_PKG_DIR="pkg"
 
-bashio::log.info "Rozpoczynam serwer RUST x2iot na porcie 8356..."
+echo "Rozpoczynam serwer RUST x2iot na porcie 8356..."
 
 exec /app/x2iot-app
